59d0966d707b1bd42a7c2bf266db9537e9ae03ac
Iman Salehi <cse.isalehi@gmail.com>
2024-09-10 14:02:10 +0200

    init project

---
+++ src/main/java/be/jimsa/reddoctor/RedDoctorApplication.java
@@ -0,0 +1,13 @@
+package be.jimsa.reddoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedDoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedDoctorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/jimsa/reddoctor/RedDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jimsa.reddoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
