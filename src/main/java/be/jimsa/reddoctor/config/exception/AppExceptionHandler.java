4f405eb5075d8d90985e33dcf63e7a8a30662182
Iman Salehi <cse.isalehi@gmail.com>
2024-09-19 12:12:09 +0330

    update AppExceptionHandler

--- src/main/java/be/jimsa/reddoctor/config/exception/AppExceptionHandler.java
+++ src/main/java/be/jimsa/reddoctor/config/exception/AppExceptionHandler.java
@@ -6,9 +6,7 @@ import jakarta.persistence.OptimisticLockException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
@@ -17,8 +15,6 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -31,13 +27,11 @@ import static be.jimsa.reddoctor.utility.constant.ProjectConstants.*;
 
 @RestControllerAdvice
 @Slf4j
-public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+public class AppExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_METHOD_ARGUMENT_NOT_VALID, ex.getMessage())
-        );
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_METHOD_ARGUMENT_NOT_VALID, ex.getMessage());
         Map<String, String> hashMap = new HashMap<>();
         List<ObjectError> errors = ex.getBindingResult().getAllErrors();
         errors.forEach(error -> {
@@ -45,8 +39,8 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                         hashMap.put(((FieldError) error).getField(),
                                 error.getDefaultMessage()
                         );
-                    } else if (error != null) {
-                        String message = error.getDefaultMessage();
+                    } else if (error != null && error.getDefaultMessage() != null) {
+                        String message = messageExtractor(error.getDefaultMessage());
                         String start = Objects.requireNonNull(error.getArguments())[1].toString();
                         String end = error.getArguments()[2].toString();
                         hashMap.put(start + "-" + end, message);
@@ -64,32 +58,28 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                 );
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_HTTP_MESSAGE_NOT_READABLE, ex.getMessage())
-        );
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_HTTP_MESSAGE_NOT_READABLE, ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(
                         ResponseDto.builder()
                                 .action(false)
                                 .timestamp(LocalDateTime.now())
-                                .result(ex.getMostSpecificCause().getMessage())
+                                .result(messageExtractor(ex.getMostSpecificCause().getMessage()))
                                 .build()
                 );
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ResponseDto> handleConstraintViolationExceptions(ConstraintViolationException ex) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_CONSTRAINT_VIOLATION_EXCEPTION, ex.getMessage())
-        );
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_CONSTRAINT_VIOLATION_EXCEPTION, ex.getMessage());
         Map<String, String> hashMap = new HashMap<>();
         ex.getConstraintViolations().forEach(violation -> {
                     String fullPath = violation.getPropertyPath().toString();
                     String fieldName = fullPath.substring(fullPath.lastIndexOf('.') + 1);
-                    hashMap.put(fieldName, violation.getMessage());
+                    hashMap.put(fieldName, messageExtractor(violation.getMessage()));
                 }
         );
         return ResponseEntity
@@ -108,11 +98,9 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
             ObjectOptimisticLockingFailureException.class
     })
     public ResponseEntity<ResponseDto> handleOptimisticLockException(OptimisticLockException ex, HttpServletRequest webRequest) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_OPTIMISTIC_LOCK_EXCEPTION, ex.getMessage())
-        );
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_OPTIMISTIC_LOCK_EXCEPTION, ex.getMessage());
         Map<String, String> hashMap = new HashMap<>();
-        hashMap.put(GENERAL_EXCEPTION_MESSAGE, ex.getMessage());
+        hashMap.put(GENERAL_EXCEPTION_MESSAGE, messageExtractor(ex.getMessage()));
         hashMap.put(GENERAL_EXCEPTION_PATH, String.format(GENERAL_EXCEPTION_REGEX, webRequest.getMethod(), webRequest.getRequestURI()));
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
@@ -127,11 +115,9 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = AppServiceException.class)
     public ResponseEntity<ResponseDto> handleAppServiceExceptions(AppServiceException ex, HttpServletRequest webRequest) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_APP_SERVICE_EXCEPTION, ex.getMessage())
-        );
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_APP_SERVICE_EXCEPTION, ex.getMessage());
         Map<String, String> hashMap = new HashMap<>();
-        hashMap.put(GENERAL_EXCEPTION_MESSAGE, ex.getMessage());
+        hashMap.put(GENERAL_EXCEPTION_MESSAGE, messageExtractor(ex.getMessage()));
         hashMap.put(GENERAL_EXCEPTION_PATH, String.format(GENERAL_EXCEPTION_REGEX, webRequest.getMethod(), webRequest.getRequestURI()));
         return ResponseEntity
                 .status(ex.getHttpStatus())
@@ -146,11 +132,9 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ResponseDto> handleExceptions(Exception ex, HttpServletRequest webRequest) {
-        log.error(
-                String.format(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_APP_500_EXCEPTION, ex.getMessage())
-        );
+        log.error(GENERAL_EXCEPTION_LOG_PATTERN, EXCEPTION_METHOD_APP_500_EXCEPTION, ex.getMessage());
         Map<String, String> hashMap = new HashMap<>();
-        hashMap.put(GENERAL_EXCEPTION_MESSAGE, ex.getMessage());
+        hashMap.put(GENERAL_EXCEPTION_MESSAGE, messageExtractor(ex.getMessage()));
         hashMap.put(GENERAL_EXCEPTION_PATH, String.format(GENERAL_EXCEPTION_REGEX, webRequest.getMethod(), webRequest.getRequestURI()));
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -161,5 +145,12 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                                 .result(hashMap)
                                 .build()
                 );
+    }
+
+    private String messageExtractor(String ex) {
+        if (ex.contains(GENERAL_EXCEPTION_NULL)) {
+            return GENERAL_EXCEPTION_NULL_MESSAGE;
+        }
+        return ex;
     }
 }
