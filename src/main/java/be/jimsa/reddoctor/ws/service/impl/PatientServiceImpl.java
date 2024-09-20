2b7890c7c0aff0bbdc247751ade5f6c3867718c0
Iman Salehi <cse.isalehi@gmail.com>
2024-09-20 17:25:34 +0330

    update PatientServiceImpl

--- src/main/java/be/jimsa/reddoctor/ws/service/impl/PatientServiceImpl.java
+++ src/main/java/be/jimsa/reddoctor/ws/service/impl/PatientServiceImpl.java
@@ -11,7 +11,6 @@ import be.jimsa.reddoctor.ws.model.enums.Status;
 import be.jimsa.reddoctor.ws.repository.AppointmentRepository;
 import be.jimsa.reddoctor.ws.repository.PatientRepository;
 import be.jimsa.reddoctor.ws.service.PatientService;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,6 +18,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,7 +52,9 @@ public class PatientServiceImpl implements PatientService {
         }
 
         Optional<Patient> optionalPatient = patientRepository.findByPhoneNumber(patientDto.getPhoneNumber());
-        Patient savedPatient = optionalPatient.map(patient -> patientRepository.save(patientMapper.mapToEntityById(patientDto, patient.getId()))).orElseGet(() -> patientRepository.save(patientMapper.mapToEntity(patientDto)));
+        Patient savedPatient = optionalPatient
+                .map(patient -> patientRepository.save(patientMapper.mapToEntityById(patientDto, patient.getId())))
+                .orElseGet(() -> patientRepository.save(patientMapper.mapToEntity(patientDto)));
 
         Appointment appointment = optionalAppointment.get();
         appointment.setPatient(savedPatient);
